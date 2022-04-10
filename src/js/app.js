4f8d7758f4831e881f8e4c5016fe13616ee65ef6
Lisa Huang <LisaHuangAde19@gmail.com>
2022-04-10 23:17:01 +0930

    html + src：js 　type=module

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+// 引入 m1.js m2 m3 模块内容
+import * as m1 from './m1.js'
+import { school as gui, findJob } from './m2.js'
+import m3 from "./m3.js"
+
+console.log(m1)
+console.log(gui, findJob)
+console.log(m3)
